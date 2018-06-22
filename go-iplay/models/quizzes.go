3913f9d46b2caa97f66cec748ec0afd739ad35e2
dreamflyfengzi <fengzi.dreamfly@gmail.com>
2018-06-21 19:23:54 -0700

    add choice of quizzes

--- go-iplay/models/quizzes.go
+++ go-iplay/models/quizzes.go
@@ -11,4 +11,13 @@ type Quizzes struct {
 	Begin       time.Time // 竞猜开始时间
 	End         time.Time // 竞猜结束时间
 	Created     time.Time `orm:"auto_now_add;type(datetime)"`
+	choices     ChoiceOpt
+}
+
+type ChoiceOpt struct {
+	name    string  `orm:"size(512)"` // 竞猜说明
+	odds    float32 //赔率
+	percent float32 //下注比例
+	count   int64   //下注人数
+	totoal  int64   //下注总金额
 }
