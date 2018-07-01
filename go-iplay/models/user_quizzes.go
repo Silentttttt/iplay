45adb8ef424259c4dec60e8c7bf9fae74e19e4eb
Leon <leon@nebulas.io>
2018-07-01 21:45:27 +0800

    optimize user quizzes

--- go-iplay/models/user_quizzes.go
+++ go-iplay/models/user_quizzes.go
@@ -11,10 +11,16 @@ type UserQuizzes struct {
 	User      *User      `orm:"rel(fk)" json:"user"`
 	ChoiceOpt *ChoiceOpt `orm:"rel(fk)" json:"choice_opt"`
 	Quizzes   *Quizzes   `orm:"rel(fk)" json:"quizzes"`
+	Game      *Game      `orm:"rel(fk)" json:"-"`
 	Result    bool       `json:"result"`
 	Money     int64      `json:"money"`  // 下注金额
 	Reward    int64      `json:"reward"` // 竞猜奖励
 	Created   time.Time  `orm:"auto_now_add;type(datetime)" json:"-"`
+}
+
+type GameUserQuizzes struct {
+	GameId int64
+	Count  int
 }
 
 func (uq *UserQuizzes) TableName() string {
@@ -28,4 +34,26 @@ func GetUserQuizzesList(userID int64) (*[]UserQuizzes, error) {
 		return nil, err
 	}
 	return &quizzes, nil
+}
+
+func GetUserQuizzesListByGame(userID int64, gameID int64) (*[]UserQuizzes, error) {
+	quizzes := []UserQuizzes{}
+	_, err := orm.NewOrm().QueryTable(UserQuizzesTBName()).Filter("user_id", userID).Filter("game_id", gameID).RelatedSel().All(&quizzes)
+	if err != nil {
+		return nil, err
+	}
+	return &quizzes, nil
+}
+
+func GetUserQuizzesGroupByGame(userID int64, gameId int64) (*[]GameUserQuizzes, error) {
+	var gameUserQuizzes []GameUserQuizzes
+	o := orm.NewOrm()
+	if gameId > 0 {
+		return &[]GameUserQuizzes{GameUserQuizzes{GameId: gameId, Count: 1}}, nil
+	}
+	_, err := o.Raw("SELECT game_id, count(*) FROM tb_user_quizzes WHERE user_id = ? group by game_id", userID).QueryRows(&gameUserQuizzes)
+	if err != nil {
+		return nil, err
+	}
+	return &gameUserQuizzes, err
 }
