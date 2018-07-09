411607465035745c496a683eedfac8536305da28
dreamflyfengzi <fengzi.dreamfly@gmail.com>
2018-07-09 21:09:21 +0800

    update user.js

--- webapp/js/game.js
+++ webapp/js/game.js
@@ -1,116 +1,151 @@
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+class Game{
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
+    constructor(userObj){
+        this.userObj = userObj;
+
+        var game_id = this.getUrlParameter("game_id");
+        this.game_id = parseInt(game_id)
+
+        this.user_id = this.userObj.getUserId();
+
+        this.choice_opt = {
+            choice_id: 0,
+            quizzes_id: 0,
+            odds: 1,
+            name: "",
+            bet: 0,
         }
     }
-};
 
-jQuery(document).ready(function ($) {
+    onLoad(){
 
-    // 打开  好友pk
-    $("#friend_play").click(function () {
-        $("#haoyoupk_bgc").fadeIn(100);
-        $('#haoyoupk_banner').slideDown(200);
-    })
-
-    // 关闭 好友pk
-    $('#friend_play .close').click(function () {
-        $('#haoyoupk_bgc').fadeOut(100);
-        $('#haoyoupk_banner').slideUp(200);
-    })
-
-    // 关闭下注
-    $('#bet_banner .close').click(function () {
-        $('#bet_bgc').fadeOut(100);
-        $('#bet_banner').slideUp(200);
-    })
-
-
-
-    var game_id = getUrlParameter("game_id");
-    game_id = parseInt(game_id)
-
-    $(".game-card-tab-item").click(function () { 
-        $(".game-card-tab-item").removeClass("mui-active")
-        $(this).addClass("mui-active");
-       
-        $(".game-card-content-item").addClass("hide")
-        var tab_item = $(this).attr("tab-item");
-        $("#"+tab_item).removeClass("hide");
-        console.log("#"+tab_item)
-    })
-
-
-    var choice_opt = {
-        choice_id: 0,
-        quizzes_id: 0,
-        odds: 1,
-        name: "",
-        bet: 0,
+        this.getGameInfo();
+        this.showMyQuizzesChoice();
+        this.onEvent()
     }
 
-    var odds = 1;
-    var name = "";
-    $("#guess-items").on("click", ".quizzes-choice-item", function () {
-        choice_opt.name = $(this).attr("quizzes-choice-item-name");
-        choice_opt.odds = $(this).attr("quizzes-choice-item-odds");
-        choice_opt.choice_id = $(this).attr("quizzes-choice-item-id");
-
-        choice_opt.quizzes_id = $(this).parents(".quizzes-choice-items").attr("quizzes-id");
-
-        console.log($(this).parents(".quizzes-choice-items"))
-        choice_opt.bet = 0;
-        console.log(name, odds, choice_opt)
-
-        $('#game-bet-name').html("投注: " + choice_opt.name);
-        $('#game-bet-odds').html(choice_opt.odds);
-        $('#game-bet-reward').html(choice_opt.odds * choice_opt.bet + 'QB');
-
-        $('#bet_bgc').fadeIn(100);
-        $('#bet_banner').slideDown(100);
-    })
-
+    getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
     
-    $(".game-bet-change-money").click(function () {
-
-        choice_opt.bet = $(this).attr("bet-money");
-
-        $(this).addClass("mui-active");
-
-        console.log(this)
-
-        $('#game-bet-reward').html(parseInt(choice_opt.odds * choice_opt.bet) + 'QB');
-    })
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
     
-
-
-    $("#game-quizz-bet").click(function () {
-
-        var user_id = parseInt($.cookie('user_id'));
-        if (user_id == 0) {
-            alert("请登录");
-            window.location.href = "login.html";
-            return;
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
         }
-        if (choice_opt.bet<=0){
-            alert("请选择下注额度");
+    }
+
+
+    onEvent(){
+         // 打开  好友pk
+        $("#friend_play").click(function () {
+            $("#haoyoupk_bgc").fadeIn(100);
+            $('#haoyoupk_banner').slideDown(200);
+        })
+
+        // 关闭 好友pk
+        $('#friend_play .close').click(function () {
+            $('#haoyoupk_bgc').fadeOut(100);
+            $('#haoyoupk_banner').slideUp(200);
+        })
+
+        // 关闭下注
+        $('#bet_banner .close').click(function () {
+            $('#bet_bgc').fadeOut(100);
+            $('#bet_banner').slideUp(200);
+        })
+
+        $(".game-card-tab-item").click(function () { 
+            $(".game-card-tab-item").removeClass("mui-active")
+            $(this).addClass("mui-active");
+           
+            $(".game-card-content-item").addClass("hide")
+            var tab_item = $(this).attr("tab-item");
+            $("#"+tab_item).removeClass("hide");
+            console.log("#"+tab_item)
+        })
+
+
+        $('.theme-popover-mask').click(function () {
+            $('.theme-popover-mask').fadeOut(100);
+            $('.bet-theme-popover').slideUp(100);
+        })
+
+        this.onEventBet()
+    }
+
+
+    onEventBet(){
+        var that = this
+        var odds = 1;
+        var name = "";
+        $("#guess-items").on("click", ".quizzes-choice-item", function () {
+            that.choice_opt.name = $(this).attr("quizzes-choice-item-name");
+            that.choice_opt.odds = $(this).attr("quizzes-choice-item-odds");
+            that.choice_opt.choice_id = $(this).attr("quizzes-choice-item-id");
+    
+            that.choice_opt.quizzes_id = $(this).parents(".quizzes-choice-items").attr("quizzes-id");
+    
+            console.log($(this).parents(".quizzes-choice-items"))
+            that.choice_opt.bet = 0;
+            console.log(name, odds, that.choice_opt)
+    
+            $('#game-bet-name').html("投注: " + that.choice_opt.name);
+            $('#game-bet-odds').html(that.choice_opt.odds);
+            $('#game-bet-reward').html(that.choice_opt.odds * that.choice_opt.bet + 'QB');
+    
+            $('#bet_bgc').fadeIn(100);
+            $('#bet_banner').slideDown(100);
+        })
+    
+        
+        $(".game-bet-change-money").click(function () {
+    
+            that.choice_opt.bet = $(this).attr("bet-money");
+    
+            $(this).addClass("mui-active");
+    
+            console.log(this)
+    
+            $('#game-bet-reward').html(parseInt(that.choice_opt.odds * that.choice_opt.bet) + 'QB');
+        })
+
+        $("#game-quizz-bet").click(function () {
+            that.doBet()
+        })
+    };
+
+
+    doBet() {
+
+        if(!this.userObj.isLogin()){
+            var btnArray = ['否', '是'];
+            mui.confirm('还未登录，请登录', '请登录', btnArray, function(e) {
+                if (e.index == 1) {
+                    window.location.href = "login.html";
+                } else {
+                    
+                }
+            })
+            return 
+        }
+        
+        if (this.choice_opt.bet<=0){
+            mui.toast('请选择下注额度',{ duration:'long', type:'div' });
             return;
         }
         var params = {
-            game_id: game_id ? parseInt(game_id) : 0,
-            "auth_token": $.cookie('auth_token'),
-            "bet_amount": parseInt(choice_opt.bet),
-            "choice_opt_id": parseInt(choice_opt.choice_id),
-            "quizzes_id": parseInt(choice_opt.quizzes_id),
-            "user_id": user_id
+            game_id: this.game_id,
+            "auth_token": this.userObj.getAuthToken(),
+            "bet_amount": parseInt(this.choice_opt.bet),
+            "choice_opt_id": parseInt(this.choice_opt.choice_id),
+            "quizzes_id": parseInt(this.choice_opt.quizzes_id),
+            "user_id": this.user_id
         };
         var url = "http://35.180.103.230:8080/v1/user/do_quizzes";
 
@@ -136,7 +171,7 @@ jQuery(document).ready(function ($) {
                     //showMyQuizzesChoice(game_id)
                     $('.theme-popover-mask').fadeOut(100);
                     $('.bet-theme-popover').slideUp(200);
-                   
+                    
                 } else if (res.code == 501) {
                     alert("请先登录");
                     window.location.href = "login.html"
@@ -145,14 +180,16 @@ jQuery(document).ready(function ($) {
                 }
             }
         });
-    })
+    }
 
-    function load() {
+
+    getGameInfo() {
         var params = {
-            game_id: game_id ? parseInt(game_id) : 1
+            game_id: this.game_id
         };
         var url = "http://35.180.103.230:8080/v1/game/quizzes";
 
+        var that = this
         $.ajax({
             type: "POST",
             url: url,
@@ -161,21 +198,19 @@ jQuery(document).ready(function ($) {
             dataType: "json",
             success: function (res) {
                 console.log(res)
-                showGame(res.data.game)
+                that.showGame(res.data.game)
 
-                showQuizzes(res.data.quizzes)
+                that.showQuizzes(res.data.quizzes)
             }
         });
     }
 
-
-    function showMyQuizzesChoice(game_id) {
-        var user_id = parseInt($.cookie('user_id'));
+    showMyQuizzesChoice() {
 
         var params = {
-            "auth_token": $.cookie('auth_token'),
-            "user_id": user_id,
-            "game_id": game_id,
+            "auth_token": this.userObj.getAuthToken(),
+            "user_id": this.user_id,
+            "game_id": this.game_id,
         };
         var url = "http://35.180.103.230:8080/v1/user/quizzes_list";
 
@@ -197,7 +232,7 @@ jQuery(document).ready(function ($) {
         });
     }
 
-    function showGame(game) {
+    showGame(game) {
 
         document.title = game.home_team.name + 'VS' + game.visit_team.name;
 
@@ -206,15 +241,9 @@ jQuery(document).ready(function ($) {
         $(".game-card-tab").show();
     }
 
-    function showQuizzes(quizzes) {
+    showQuizzes(quizzes) {
         $("#game-quizz-tpl").tmpl(quizzes).appendTo("#guess-items");
     }
 
-    $('.theme-popover-mask').click(function () {
-        $('.theme-popover-mask').fadeOut(100);
-        $('.bet-theme-popover').slideUp(100);
-    })
-
-    load()
-    showMyQuizzesChoice(game_id)
-})
+    
+}
